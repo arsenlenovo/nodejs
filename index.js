cc482d20f6d7aa14ec6ae5e10b2cf4d85baff056
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-01-18 15:08:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let myName = "Arsen";
+console.log(myName);
+console.log("My name's length is " + myName.length)
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());
+
+let pi = 3.141593
+console.log(pi);
+console.log(pi.toFixed(2));
+console.log(pi.toFixed(0));
+
+let example1 = 2 + 2; 
+let example2 = 2 - 2;
+let example3 = 2 * 2; //12
+let example4 = 2 / 2; //12
+let example6 = 2 ** 4;
+let example7 = 2 % 2;
+let example8 = 81 ** (1/2);
+let example9 = "Hello " + 'World' + `!`;
+let example10 = 4 + 7 + 12;
+let example11 = 1 + 5 * 3;
+let example12 = 15 * (1 - 1);
+
+let example13 = 5 + '3';
+let example14 = '5' + '3';
+let example15 = 'a' * 3; //15
+let example16 = 'a' * 3; //NaN 
+let example17 = -15;
+let example18 = +'15';
+
+console.log(example18); 
